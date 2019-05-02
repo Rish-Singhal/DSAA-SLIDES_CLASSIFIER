442e5b41e3ce8e349590a46d408447fd8955acbe
Rish-Singhal <zzxy334@gmail.com>
2019-05-02 14:16:30 +0530

    updated main with sift

--- main.py
+++ main.py
@@ -5,20 +5,29 @@ import cv2
 dirname = sys.argv[1:]
 numArg = len(dirname)
 
-# print(dirname)
-
 if numArg != 2:
     print("syntax: python3 main.py path_dir1 path_dir2")
     exit(0)
+
+
+sift = cv2.xfeatures2d.SIFT_create()
 
 slides = []
 for files in os.listdir(dirname[0]):
     slide = cv2.imread(os.path.join(dirname[0], files))
     if slide is not None:
-        slides.append(slide)
+        gslide = cv2.cvtColor(slide, cv2.COLOR_BGR2GRAY)
+        keyPoint = sift.detect(gslide, None)
+        img = cv2.drawKeypoints(
+            gslide, keyPoint, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        slides.append(img)
 
 yslides = []
 for files in os.listdir(dirname[1]):
-    slide = cv2.imread(os.path.join(dirname[1], files))
+    yslide = cv2.imread(os.path.join(dirname[1], files))
     if slide is not None:
-        yslides.append(slide)
+        gslide = cv2.cvtColor(yslide, cv2.COLOR_BGR2GRAY)
+        keyPoint = sift.detect(gslide, None)
+        img = cv2.drawKeypoints(
+            gslide, keyPoint, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        yslides.append(img)
